71197d386d43c9e40fa6016801d0d8ae21fd4744
John Nelson <jnelson@johncoder.com>
2020-08-18 10:52:24 -0400

    keybinds + env functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useRef} from 'react';
+import {generateKeybinds, useEventListener} from './keybindings';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [panelVisible, setPanelVisible] = useState(false);
+  const [functionSelectVisible, setFunctionSelectVisible] = useState(false);
+  const [functionSelectInput, setFunctionSelectInput] = useState("");
+
+  // thinking: env stores published functions
+  const env = {};
+
+  const functionSelectInputRef = useRef(null);
+  
+  const reset = () => setCount(0);
+  const togglePanelVisible = () => setPanelVisible(!panelVisible);
+  const incrementCount = () => setCount(count + 1);
+  const toggleFunctionSelectVisible = () => {
+    const newValue = !functionSelectVisible;
+    setFunctionSelectVisible(newValue);
+    if (newValue) {
+      functionSelectInputRef.current.focus();
+    }
+  };
+  const updateFunctionSelectInput = (e) => setFunctionSelectInput(e.target.value);
+  const executeSelectedFunction = () => {
+    let fn = functionSelectInput;
+    const candidates = Object.keys(env)
+          .filter(e => e.startsWith(functionSelectInput));
+    if (candidates.length === 1) {
+      fn = candidates[0];
+    }
+    if (env.hasOwnProperty(fn) &&
+        env[fn] instanceof Function) {
+      env[fn]();
+    }
+    setFunctionSelectVisible(false);
+    setFunctionSelectInput("");
+  };
+
+  env['reset-counter'] = reset;
+  env['toggle-panel-visible'] = togglePanelVisible;
+  env['increment-counter'] = incrementCount;
+  env['toggle-function-select'] = toggleFunctionSelectVisible;
+
+  const [handler] = generateKeybinds([
+    {
+      keys: {key: 'Escape'},
+      action: reset,
+    },
+    {
+      keys: {key: 'c', ctrlKey: true},
+      action: togglePanelVisible,
+    },
+    {
+      keys: {key: 'g'},
+      action: incrementCount,
+      // TODO(john): improve this?
+      only: () => document.activeElement === document.body,
+    },
+    {
+      keys: {key: 'x', altKey: true},
+      action: toggleFunctionSelectVisible,
+    },
+    {
+      keys: {key: 'Enter'},
+      action: executeSelectedFunction,
+      // TODO(john): improve this?
+      only: () => document.activeElement === functionSelectInputRef.current,
+    }
+  ]);
+  useEventListener('keydown', handler);
+
+  const handleInputEnter = (e) => {
+    if (String(e.key) === 'Return') {
+      executeSelectedFunction();
+    }
+  };
+
+  const fnSelect = (
+    <div className="functionSelect">
+      <input name="functionSelectInputValue" type="text"
+             value={functionSelectInput}
+             autoComplete="off"
+             onChange={updateFunctionSelectInput}
+             onKeyPress={handleInputEnter}
+             ref={functionSelectInputRef}/>
+      <ul>
+        {Object.keys(env)
+         .filter(e => e.startsWith(functionSelectInput))
+         .map((e, i) => (<li key={i}>{e}</li>))
+         .slice(0, 5)}
+      </ul>
+    </div>
+  );
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {panelVisible &&
+         <p>
+          Hello, world! {count}
+         </p>
+        }
+        {functionSelectVisible && fnSelect}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Experience React
         </a>
       </header>
     </div>
---
+++ src/keybindings.js
@@ -0,0 +1,102 @@
+import { useEffect, useRef } from 'react';
+
+/*
+a keybindings module that:
++ allows components to publish functions
++ allows components to associate keybindings to functions
+*/
+
+function useEventListener(eventName, handler, element = window) {
+  const savedHandler = useRef();
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(
+    () => {
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) {
+        return () => {
+        };
+      }
+
+      const eventListener = event => savedHandler.current(event);
+
+      element.addEventListener(eventName, eventListener);
+
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element]
+  );
+}
+
+function createTrieNode(key, terminate, children) {
+  return {
+    key,
+    terminate,
+    children: [],
+  };
+}
+
+function createTrie() {
+  const root = createTrieNode();
+  
+  return {
+    insert: (s) => {
+      if (!s || s.length) {
+        return;
+      }
+      if (!root) {
+        return;
+      }
+    },
+    contains: () => {
+    },
+    next: () => {
+    },
+  };
+}
+
+function isKey(left, right) {
+  if (left.key !== String(right.key)) {
+    return false;
+  }
+  if (left.hasOwnProperty('ctrlKey') && left.ctrlKey !== right.ctrlKey) {
+    return false;
+  }
+
+  if (left.hasOwnProperty('altKey') && left.altKey !== right.altKey) {
+    return false;
+  }
+  return true;
+}
+
+function generateKeybinds(keybindings) {
+  createTrie(); // TODO(john): not this
+
+  // NOTE(john): for now we can just do this the dumb way
+  function handler(e) {
+    const kbs = keybindings
+          .filter(kb => isKey(kb.keys, e));
+    if (!kbs || kbs.length === 0) {
+      // console.log(e, kbs);
+      return;
+    }
+
+    const k = kbs[0];
+    console.log(k);
+    if (!k.hasOwnProperty('only') || k.only()) {
+      k.action();
+      e.preventDefault();
+    }
+    return;
+  }
+  return [handler];
+}
+
+export {
+  generateKeybinds,
+  useEventListener,
+};
